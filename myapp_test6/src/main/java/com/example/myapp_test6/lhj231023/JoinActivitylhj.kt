3dfc1b40df641fcba4ab7565f59adcd4b8116401
lhj257 <hojin159753@naver.com>
2023-10-23 22:08:03 +0900

    과제중

--- myapp_test6/src/main/java/com/example/myapp_test6/lhj231023/JoinActivitylhj.kt
+++ myapp_test6/src/main/java/com/example/myapp_test6/lhj231023/JoinActivitylhj.kt
@@ -3,10 +3,16 @@ package com.example.myapp_test6.lhj231023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapp_test6.R
+import com.example.myapp_test6.databinding.ActivityJoinBinding
 
 class JoinActivitylhj : AppCompatActivity() {
+    private lateinit var binding: ActivityJoinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join_activitylhj)
+
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
