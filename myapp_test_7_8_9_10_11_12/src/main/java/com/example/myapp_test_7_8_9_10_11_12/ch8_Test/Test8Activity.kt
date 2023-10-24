937f5e40d1ca4ac2c859d8c8b38ea3e6396e0357
lsy3709 <lsy3709@naver.com>
2023-10-24 16:39:18 +0900

    231024 7교시  Test8Activity - 터치 이벤트 리스너 sam 방식

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch8_Test/Test8Activity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch8_Test/Test8Activity.kt
@@ -2,6 +2,7 @@ package com.example.myapp_test_7_8_9_10_11_12.ch8_Test
 
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +11,7 @@ import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest8Binding
 class Test8Activity : AppCompatActivity() {
     lateinit var activityTest8Binding: ActivityTest8Binding
     lateinit var TAG:String
+    ////////////////////////////////////////////////////////////////////////////////////
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TAG ="lsy"
@@ -20,8 +22,15 @@ class Test8Activity : AppCompatActivity() {
         // 모두 담은 인스턴스의 부모 레이아웃을 출력을 함.
         setContentView(activityTest8Binding.root)
 
-    }
+        // test 버튼 클릭 리스너 이벤트 처리 SAM 기법 (Sing Abstract Method) ,
+    //  보통 자바에서, 람다식을 연결할 때 사용하는 인터페이스
+    //  함수형 인터페이스, 추상 메서드가 하나인 인터페이스
+        activityTest8Binding.testBtn.setOnClickListener {
+            Toast.makeText(this@Test8Activity,"test !!!!",Toast.LENGTH_SHORT).show()
+        }
 
+    }
+    ////////////////////////////////////////////////////////////////////////////////////
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when(event?.action) {
             MotionEvent.ACTION_DOWN -> {
@@ -44,6 +53,16 @@ class Test8Activity : AppCompatActivity() {
 
 
     }
+////////////////////////////////////////////////////////////////////////////////////
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+    when(keyCode){
+        KeyEvent.KEYCODE_0 -> Log.d(TAG,"0 키 누름")
+        KeyEvent.KEYCODE_A -> Log.d(TAG,"A 키 누름")
+        KeyEvent.KEYCODE_ENTER -> Log.d(TAG,"enter 키 누름")
 
+
+    }
+        return super.onKeyDown(keyCode, event)
+    }
 
 }
