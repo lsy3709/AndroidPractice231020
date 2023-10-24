394b6741f520203d2ffe9b08d20f3731be571542
lsy3709 <lsy3709@naver.com>
2023-10-24 10:13:22 +0900

    231024 2교시-1  TestActivity - Toast 메세지 출력

--- myapp_test6/src/main/java/com/example/myapp_test6/TestActivity.kt
+++ myapp_test6/src/main/java/com/example/myapp_test6/TestActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myapp_test6
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapp_test6.databinding.ActivityMainBinding
 import com.example.myapp_test6.databinding.ActivityTestBinding
 
 class TestActivity : AppCompatActivity() {
@@ -22,11 +24,36 @@ class TestActivity : AppCompatActivity() {
 
         //showBtn -> 클릭시 , 프로필 이미지 profileImg , 보이게
         // 특정 뷰를 선택하는 방법 2가지.
-        // 1) findViewById<>() 를 이용하는 방법
+        // 1)-1 findViewById<>() 를 이용하는 방법
         val showBtn:Button = findViewById(R.id.showBtn)
-        // 2) viewBinding 를 이용하는 방법
-        val showBtn = activityMainBinding.
+        // 2)-1 viewBinding 를 이용하는 방법
+        val showBtn2 = activityTestBinding.showBtn
+
+        // 선택한 뷰를 클릭 이벤트 핸들러를 추가.
+        //1)-2
+        showBtn.setOnClickListener {
+            val profilImg : ImageView = findViewById(R.id.profileImg)
+            profilImg.visibility = View.VISIBLE
+        }
 
         // hideBtn -> 클릭시 , 프로필 이미지 profileImg , 사라짐.
+        //1)-3
+        // 특정 뷰를 선택하는 방법 2가지.
+        // 1)-1 findViewById<>() 를 이용하는 방법
+        val hideBtn:Button = findViewById(R.id.hideBtn)
+
+        hideBtn.setOnClickListener {
+            val profilImg : ImageView = findViewById(R.id.profileImg)
+            profilImg.visibility = View.INVISIBLE
+        }
+
+        // 2)-1 viewBinding 를 이용하는 방법
+        val hideBtn2 = activityTestBinding.hideBtn
+
+        val profileImgView : ImageView = findViewById(R.id.profileImg)
+        profileImgView.setOnClickListener {
+            // 매개변수 1번: 현재 컨텍스트 : 액티비티, 2번 : 메세지 내용 , 3번: 메세지 출력 시간, 숏:3초, 롱:5초 정도
+            Toast.makeText(this@TestActivity,"이미지 클릭했음.!!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
