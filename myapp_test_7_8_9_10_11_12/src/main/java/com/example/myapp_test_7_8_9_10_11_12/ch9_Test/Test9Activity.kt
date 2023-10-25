aceb9449b5bc4ee278ce0bdc21ec845d1d72193f
lsy3709 <lsy3709@naver.com>
2023-10-25 12:23:17 +0900

    231025 4교시 10 퍼미션 셀프 체크 위치 접근 허용여부 확인.

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch9_Test/Test9Activity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch9_Test/Test9Activity.kt
@@ -1,9 +1,14 @@
 package com.example.myapp_test_7_8_9_10_11_12.ch9_Test
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowMetrics
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.myapp_test_7_8_9_10_11_12.R
 import com.example.myapp_test_7_8_9_10_11_12.ch7_Test.TestActivity
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest9Binding
@@ -26,6 +31,29 @@ class Test9Activity : AppCompatActivity() {
         }
         // 소스 코드로 , 정적 자원 사용하기 . 문자열.
         activityTest9Binding.textView2.text = getString(R.string.app_intro)
+
+        // 해당 라이브러리의 기능을 확인 하는 부분 보다.
+        // sdk 버전에 따라서, 라이브러리 패키지 명 등, 다른 부분에 집중.
+        // 사용하는 라이브러리의 sdk 버전의 지원 여부 확인.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics: WindowMetrics =  windowManager.currentWindowMetrics
+            Log.d("lsy","width: ${windowMetrics.bounds.width()}, height: ${windowMetrics.bounds.height()}")
+        } else {
+            val display = windowManager.defaultDisplay
+        }
+
+        // 허가 확인 여부 테스트
+        val status = ContextCompat.checkSelfPermission(this@Test9Activity,
+            "android.permission.ACCESS_FINE_LOCATION")
+
+        if(status == PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this@Test9Activity,"위치 권한 승인됨",Toast.LENGTH_SHORT).show()
+            Log.d("lsy","권한이 승인됨 : ${status}")
+
+        } else {
+            Toast.makeText(this@Test9Activity,"위치 권한 승인안됨",Toast.LENGTH_SHORT).show()
+            Log.d("lsy","권한이 승인안됨 : ${status}")
+        }
 
 
     }
