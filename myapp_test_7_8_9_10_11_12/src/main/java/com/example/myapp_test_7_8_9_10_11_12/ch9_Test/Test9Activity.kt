249cb6be274bd37693764a492b7fb46a6c875af2
lsy3709 <lsy3709@naver.com>
2023-10-25 12:35:45 +0900

    231025 4교시 10 퍼미션 후처리를 이용해서 위치 접근 허용여부 확인.

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch9_Test/Test9Activity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch9_Test/Test9Activity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.WindowMetrics
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.myapp_test_7_8_9_10_11_12.R
@@ -54,6 +55,24 @@ class Test9Activity : AppCompatActivity() {
             Toast.makeText(this@Test9Activity,"위치 권한 승인안됨",Toast.LENGTH_SHORT).show()
             Log.d("lsy","권한이 승인안됨 : ${status}")
         }
+
+        // 후처리, 인텐트 기본적인 사용법 단순 페이지 이동만 사용했음.
+        // 인텐트 ? -> 시스템에 메세지를 전달자.
+        // 예) 페이지 이동 같은 경우, 시스템에 요청해서, 이동했음.
+        // 예2) 특정 앱에 접근을 해서, 데이터를 가져오는 작업 (= 후처리)
+        // 설정 1)
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission() ) {
+            isGranted ->
+            if(isGranted) {
+                Log.d("lsy","권한이 승인됨 , call back 후처리 요청. ")
+            } else {
+                Log.d("lsy","권한이 승인안됨 , call back 후처리 요청. ")
+            }
+        }
+        // 이용
+        requestPermissionLauncher.launch("android.permission.ACCESS_FINE_LOCATION")
+
 
 
     }
