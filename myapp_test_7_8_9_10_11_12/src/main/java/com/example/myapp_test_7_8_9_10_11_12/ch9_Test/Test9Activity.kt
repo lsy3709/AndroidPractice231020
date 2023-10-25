5a726a0f2930c86af5bd9f9e73807a403edb4a6d
lsy3709 <lsy3709@naver.com>
2023-10-25 09:52:56 +0900

    231025 1교시 9 res - 정적자원 기본 개념 1

---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch9_Test/Test9Activity.kt
@@ -0,0 +1,29 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch9_Test
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.ch7_Test.TestActivity
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest9Binding
+
+class Test9Activity : AppCompatActivity() {
+    // 바인딩의 이름 규칙은, 레이아웃 이름을 따라 갑니다.(자동생성)
+    // 단, build.gradle 에서 설정을 반드시 하고, (모듈버전에서)
+    //예) activity_test9 ->ActivityTest9Binding
+    lateinit var activityTest9Binding: ActivityTest9Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_test9)
+        activityTest9Binding = ActivityTest9Binding.inflate(layoutInflater)
+        setContentView(activityTest9Binding.root)
+
+        activityTest9Binding.testImgRetangle.setOnClickListener {
+            Toast.makeText(this@Test9Activity,"이미지클릭됨",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@Test9Activity,TestActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
