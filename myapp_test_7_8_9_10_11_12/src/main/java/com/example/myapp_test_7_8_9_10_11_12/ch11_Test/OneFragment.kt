0fb6f8d9021a2df01f8873b760de10fd512b2fc5
lsy3709 <lsy3709@naver.com>
2023-10-26 14:11:57 +0900

    231026 4교시 11 프래그먼트 1개만, 특정 위치에 출력해보기.

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/OneFragment.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/OneFragment.kt
@@ -5,56 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapp_test_7_8_9_10_11_12.R
+import com.example.myapp_test_7_8_9_10_11_12.databinding.FragmentOneBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OneFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OneFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // 베이스 액티비티에 위에 출력이 되는 조각(프래그먼트)
+    // 기본1
+    lateinit var binding : FragmentOneBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        //기본2
+        binding = FragmentOneBinding.inflate(layoutInflater,container,false)
+//        return inflater.inflate(R.layout.fragment_one, container, false)
+        // 기본3
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OneFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OneFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_FragmentActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_FragmentActivity.kt
@@ -2,11 +2,26 @@ package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.myapp_test_7_8_9_10_11_12.R
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11FragmentBinding
 
 class Test11_FragmentActivity : AppCompatActivity() {
+    // 프래그먼트를 출력하기 위한 베이스 사용.
+    lateinit var binding : ActivityTest11FragmentBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test11_fragment)
+        binding = ActivityTest11FragmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 프래그먼트 출력하기. 연결(바인딩)
+        val fragmentManager : FragmentManager = supportFragmentManager
+        val transaction : FragmentTransaction = fragmentManager.beginTransaction()
+        val oneFragment = OneFragment()
+        transaction.add(R.id.fragment1,oneFragment)
+        transaction.commit()
+
+
     }
 }
