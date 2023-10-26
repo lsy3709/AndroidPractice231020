049a420382f814f7af1f2de7928c3e1f414c4d9c
lsy3709 <lsy3709@naver.com>
2023-10-26 16:23:07 +0900

    231026 7교시 11 리사이클러 뷰 기본 개념 및, 사전 준비 작업1, 뼈대 및 목록 요소 뷰 기본구성2

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_RecyclerViewActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_RecyclerViewActivity.kt
@@ -2,11 +2,26 @@ package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapp_test_7_8_9_10_11_12.R
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapp_test_7_8_9_10_11_12.ch11_Test.recycler.MyAdapter
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11RecyclerViewBinding
 
 class Test11_RecyclerViewActivity : AppCompatActivity() {
+    lateinit var binding : ActivityTest11RecyclerViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test11_recycler_view)
+        binding = ActivityTest11RecyclerViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas = mutableListOf<String>()
+        for(i in 1..10) {
+            datas.add("라바 $i")
+        }
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        // 구분선 넣기, 나중에 옵션으로 배경이미지도 넣기 가능.
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this,LinearLayoutManager.VERTICAL))
+
     }
 }
