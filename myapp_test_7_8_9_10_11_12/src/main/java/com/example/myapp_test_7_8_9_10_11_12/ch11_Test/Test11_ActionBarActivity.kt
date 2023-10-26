bf7512b635dfdb2e8ddd99e597a1a0c4a36dc701
lsy3709 <lsy3709@naver.com>
2023-10-26 11:13:11 +0900

    231026 2교시 11 제트팩 액션바 오버플로우 메뉴 구성

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.R
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11ActionBarBinding
 
 class Test11_ActionBarActivity : AppCompatActivity() {
@@ -19,11 +21,22 @@ class Test11_ActionBarActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
+
+
+
     }
+
     // 액션바 클릭 이벤트 처리.
     override fun onSupportNavigateUp(): Boolean {
         Log.d("lsy","test")
         onBackPressed()
         return super.onSupportNavigateUp()
     }
+
+    // 액션바에 오버플로우 메뉴 붙이기
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
