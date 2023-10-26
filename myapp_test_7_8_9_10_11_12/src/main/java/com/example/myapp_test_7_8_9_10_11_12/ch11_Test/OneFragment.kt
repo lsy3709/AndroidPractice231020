d22ebfbb76ec2ce0a5cdb95e743c5265411f14e1
lsy3709 <lsy3709@naver.com>
2023-10-26 15:08:48 +0900

    231026 5교시 11 프래그먼트 생명주기 oneFragment 로 확인해보기.사전준비 작업

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/OneFragment.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/OneFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,34 @@ class OneFragment : Fragment() {
     // 베이스 액티비티에 위에 출력이 되는 조각(프래그먼트)
     // 기본1
     lateinit var binding : FragmentOneBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("lsy","생명주기,onCreate ")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lsy","생명주기,onStart ")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d("lsy","생명주기,onResume ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lsy","생명주기,onPause ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lsy","생명주기,onStop ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lsy","onDestroy, ")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
