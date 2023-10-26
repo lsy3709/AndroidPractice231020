5242703f0fcc7d9e59309d3cac5f13beaca1fbe2
lsy3709 <lsy3709@naver.com>
2023-10-26 10:51:21 +0900

    231026 2교시 11 제트팩 액션바 기본 구성

---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTest11ActionBarBinding
+
+class Test11_ActionBarActivity : AppCompatActivity() {
+    //기본1
+    lateinit var binding : ActivityTest11ActionBarBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //기본2
+        binding = ActivityTest11ActionBarBinding.inflate(layoutInflater)
+        //기본3
+        setContentView(binding.root)
+
+        //액션바에 업버튼 붙이기
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+    // 액션바 클릭 이벤트 처리.
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d("lsy","test")
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
