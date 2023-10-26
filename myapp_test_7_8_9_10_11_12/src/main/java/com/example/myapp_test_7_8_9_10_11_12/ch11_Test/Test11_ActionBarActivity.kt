1a1246f2b0b317a2608931f4a8f6a465ba99f3f8
lsy3709 <lsy3709@naver.com>
2023-10-26 11:40:17 +0900

    231026 3교시 11 제트팩 액션바 오버플로우 메뉴 이벤트 추가 제트팩 검색 위젯 사용 + 이벤트 추가, 1)검색어 변경시 마다 호출 2) 검색어가 제출시 실행되는 함수

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_ActionBarActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test_7_8_9_10_11_12.R
@@ -38,6 +39,26 @@ class Test11_ActionBarActivity : AppCompatActivity() {
     // 액션바에 오버플로우 메뉴 붙이기
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main,menu)
+
+        // 검색 뷰에, 이벤트 추가하기.
+        val menuItem = menu?.findItem(R.id.menu_main_search)
+        // menuItem 의 형을 SearchView 타입으로 변환, 형변환
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //검색어가 변경시 마다, 실행될 로직을 추가.
+                Log.d("lsy","텍스트 변경시 마다 호출 : ${newText} ")
+                return true
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어가 제출이 되었을 경우, 연결할 로직.
+                // 사용자 디비, 검색을하고, 그 결과 뷰를 출력하는 형태.
+                Toast.makeText(this@Test11_ActionBarActivity,"검색어가 전송됨 : ${query}",Toast.LENGTH_SHORT).show()
+                return true
+            }
+        })
+
         return super.onCreateOptionsMenu(menu)
     }
 
