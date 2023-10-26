bbd7aa7a426c37cb8d12ec1f413233988d26a9bb
lsy3709 <lsy3709@naver.com>
2023-10-26 15:40:04 +0900

    231026 6교시 11 리사이클러 뷰 기본 개념 및, 사전 준비 작업

---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/Test11_RecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch11_Test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.R
+
+class Test11_RecyclerViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test11_recycler_view)
+    }
+}
---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/recycler/MyAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch11_Test.recycler
+
+//뷰를 모아둔 박스
+class MyViewHolder
+
+// 뷰와 데이터 연결 한다.
+class MyAdapter {
+}
+
+// 목록 아이템의 요소 뷰를 만들기.
