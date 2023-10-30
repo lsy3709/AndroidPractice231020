85c2f1cb19c01cf1bbb0f6f1bced8bcd58a22420
lsy3709 <lsy3709@naver.com>
2023-10-30 10:36:19 +0900

    231030 1교시 11 드로워 화면의 네비게이션 헤더및 메뉴 구성하기.본문 액티비티 붙이기 작업 추가로 이벤트 핸들러 추가.

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/viewpageandrecyclerview/TestPageRecyclerActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch11_Test/viewpageandrecyclerview/TestPageRecyclerActivity.kt
@@ -36,6 +36,22 @@ class TestPageRecyclerActivity : AppCompatActivity() {
         // 3) 드로워 네비게이션 뷰 추가 설정. -> 뷰에서 작업
         // 재료 - 1) 네비게이션 헤더 2) 본문 : res -> 메뉴
 
+        // 3) 이벤트 핸들러 추가하기.
+        // 각 아이템 요소 클릭 이벤트 추가. 각 뷰마다 이벤트 핸들러가 다 다름.
+        binding.mainDrawerView.setNavigationItemSelectedListener {
+            it ->
+            if (it.title == "로그인") {
+                Toast.makeText(this@TestPageRecyclerActivity,"로그인 화면 이동",Toast.LENGTH_SHORT).show()
+            }
+           else if (it.title == "로그아웃") {
+                Toast.makeText(this@TestPageRecyclerActivity,"로그아웃 화면 이동",Toast.LENGTH_SHORT).show()
+            }
+            else if (it.title == "메인가기") {
+                Toast.makeText(this@TestPageRecyclerActivity,"메인가기 화면 이동",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+
 
 
 
