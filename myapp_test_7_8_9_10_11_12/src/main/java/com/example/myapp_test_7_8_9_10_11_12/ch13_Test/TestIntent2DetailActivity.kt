3d2215998fc63543bd56a53727ce270f82ac9537
lsy3709 <lsy3709@naver.com>
2023-10-31 13:59:22 +0900

    231031 5교시 13 인텐트 이용, 데이터 게터 세터 기본 예제2, 뷰에 입력해서 넘기기 수정작업중1

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntent2DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntent2DetailBinding
 
@@ -14,12 +15,17 @@ class TestIntent2DetailActivity : AppCompatActivity() {
 
         // 1번 화면에서 넘어온 데이터 받아서, 결과 뷰에 붙이기 작업.
         // 임시 저장소에 담기.
-        val name = intent.getStringExtra("name")
         val email = intent.getStringExtra("email")
-        val age = intent.getIntExtra("age",30)
+        val password = intent.getStringExtra("password")
+        val age = intent.getStringExtra("age")
+//        val age = intent.getIntExtra("age",30)
 
-        binding.resultNameView.text = "이름 : $name"
-        binding.resultEmailView.text = "이메일 : $email"
+        // 데이터 확인
+        Log.d("lsy","데이터 확인 2번 화면 이메일: ${email} , " +
+                "패스워드: ${password}, 나이: ${age}")
+
+        binding.resultNameView.text = "이메일 : $email"
+        binding.resultEmailView.text = "패스워드 : $password"
         binding.resultAgeView.text = "나이 : $age "
 
     }
--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -2,22 +2,23 @@ package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBinding
 
 class TestIntentActivity : AppCompatActivity() {
     // 1번 화면
     lateinit var binding : ActivityTestIntentBinding
-    var name : String = ""
     var email : String = ""
+    var password : String = ""
     var age : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       name = binding.emailEdit.text.toString()
-        email = binding.passwordEdit.text.toString()
+        email = binding.emailEdit.text.toString()
+        password = binding.passwordEdit.text.toString()
         age = binding.ageEdit.text.toString()
 
         // 인텐트에 기본 데이터 추가 및 가져오기 테스트.
@@ -25,9 +26,12 @@ class TestIntentActivity : AppCompatActivity() {
             // 데이터를 추가해서 + 화면 이동. : 내부 앱끼리 연동
             val intent = Intent(this@TestIntentActivity,
                 TestIntent2DetailActivity::class.java)
+            // 데이터 확인
+            Log.d("lsy","데이터 확인 1번 화면 이메일: ${email} , " +
+                    "패스워드: ${password}, 나이: ${age}")
             // 데이터 추가 해보기.
-            intent.putExtra("name",name)
             intent.putExtra("email",email)
+            intent.putExtra("password",password)
             intent.putExtra("age",age)
             startActivity(intent)
 
