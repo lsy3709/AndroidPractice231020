e2782d77fc42a40e746bba6a5ef79ddf77733b77
lsy3709 <lsy3709@naver.com>
2023-10-31 12:50:39 +0900

    231031 4교시 13 인텐트 이용, 데이터 게터 세터 기본 예제2, 뷰에 입력해서 넘기기

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -8,20 +8,27 @@ import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBindi
 class TestIntentActivity : AppCompatActivity() {
     // 1번 화면
     lateinit var binding : ActivityTestIntentBinding
+    var name : String = ""
+    var email : String = ""
+    var age : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+       name = binding.emailEdit.text.toString()
+        email = binding.passwordEdit.text.toString()
+        age = binding.ageEdit.text.toString()
+
         // 인텐트에 기본 데이터 추가 및 가져오기 테스트.
-        binding.testBtn1.setOnClickListener {
+        binding.testBtn2.setOnClickListener {
             // 데이터를 추가해서 + 화면 이동. : 내부 앱끼리 연동
             val intent = Intent(this@TestIntentActivity,
                 TestIntent2DetailActivity::class.java)
             // 데이터 추가 해보기.
-            intent.putExtra("name","lsy")
-            intent.putExtra("email","lsy@naver.com")
-            intent.putExtra("age",20)
+            intent.putExtra("name",name)
+            intent.putExtra("email",email)
+            intent.putExtra("age",age)
             startActivity(intent)
 
         }
