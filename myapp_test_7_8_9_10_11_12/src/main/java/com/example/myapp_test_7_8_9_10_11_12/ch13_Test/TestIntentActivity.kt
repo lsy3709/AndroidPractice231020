3909e7ed93f8d43863dd20f35033b69e9975c2e6
lsy3709 <lsy3709@naver.com>
2023-10-31 14:06:26 +0900

    231031 5교시 13 인텐트 이용, 데이터 게터 세터 기본 예제2, 뷰에 입력해서 넘기기 수정작업중3 버튼이 클릭이되는 시점에 데이터 입력값 가져오기

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -9,27 +9,36 @@ import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestIntentBindi
 class TestIntentActivity : AppCompatActivity() {
     // 1번 화면
     lateinit var binding : ActivityTestIntentBinding
-    lateinit var email : String
-    lateinit var password : String
-    lateinit var age : String
+//    lateinit var email : String
+//    lateinit var password : String
+//    lateinit var age : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTestIntentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        email = binding.emailEdit.text.toString()
-        password = binding.passwordEdit.text.toString()
-        age = binding.ageEdit.text.toString()
+//        email = binding.emailEdit.text.toString()
+//        password = binding.passwordEdit.text.toString()
+//        age = binding.ageEdit.text.toString()
 
         // 데이터 확인
-        Log.d("lsy","데이터 확인 버튼 클릭 전 0번 화면 이메일: ${email} , " +
-                "패스워드: ${password}, 나이: ${age}")
+//        Log.d("lsy","데이터 확인 버튼 클릭 전 0번 화면 이메일: ${email} , " +
+//                "패스워드: ${password}, 나이: ${age}")
 
         // 인텐트에 기본 데이터 추가 및 가져오기 테스트.
         binding.testBtn2.setOnClickListener {
             // 데이터를 추가해서 + 화면 이동. : 내부 앱끼리 연동
             val intent = Intent(this@TestIntentActivity,
                 TestIntent2DetailActivity::class.java)
+
+            // 위에서 lateinit var 선언을 하면, 화면이 최초 그려질 때,
+            // 입력이 안된 상태로 할당이 되어 버림.
+            // 그래서, 입력 란에 입력 후, 그 시점에 있는 값을
+            // 지금 , 버튼을 눌려서 전달을 하기.
+            val email = binding.emailEdit.text.toString()
+            val password = binding.passwordEdit.text.toString()
+            val age = binding.ageEdit.text.toString()
+
             // 데이터 확인
             Log.d("lsy","데이터 확인 1번 화면 이메일: ${email} , " +
                     "패스워드: ${password}, 나이: ${age}")
