b2667a5fe238f10471927cc0523b7f595fb9cadd
lsy3709 <lsy3709@naver.com>
2023-10-31 15:25:08 +0900

    231031 6교시 13 인텐트 이용 , 필터라는 속성 이용해서 외부앱에 접근 현재 구글맵을 이용함.

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestIntentActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
@@ -76,6 +77,25 @@ class TestIntentActivity : AppCompatActivity() {
            requestLauncher.launch(intent)
             // 2번화면으로 넘어감.
         }
+
+
+        // 인텐트 필터를 이용해서 외부앱에 접근 하기.
+        // 인텐트 : 시스템에 전달하는 메세지
+        // 의 속성에 intent-filter : 무엇을 하는지를 나타내는 라벨링.
+        // 시스템이 해당 필터의 요소를 보고서 해당 외부앱을 연결 시켜줌.
+        // 예) 좌표값이다. 위도 경도 -> 지도 앱(외부앱)
+        //예2) http 주소다. -> 웹 브라우저 앱  (외부앱)
+        binding.testBtn4.setOnClickListener {
+            //지도 맵 열기 테스트.
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.7749,127.4194"))
+            // 현재, 지도 관련 앱이 다양하게 없어서, 아마도 기본 앱: 구글 맵이 나옴.
+            // 만약, 지도 관련 앱이 여러 개 있다면, 특정 앱을 선택이 가능함.
+            // 사용하는 앱의 패키지명을 정확히 입력함.
+            intent.setPackage("com.google.android.apps.maps")
+            startActivity(intent)
+
+        }
+
 
         // onCreate 마지막
     }
