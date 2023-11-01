a3b40b1046300d9d1e77969675f9da0a8702bc8c
lsy3709 <lsy3709@naver.com>
2023-11-01 12:34:09 +0900

    231101 4교시 13 비동기 처리 방식 코루틴 방법2

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestCoroutineActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch13_Test/TestCoroutineActivity.kt
@@ -1,12 +1,16 @@
 package com.example.myapp_test_7_8_9_10_11_12.ch13_Test
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityTestCoroutineBinding
-import kotlin.concurrent.thread
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
 import kotlin.system.measureTimeMillis
 
 class TestCoroutineActivity : AppCompatActivity() {
@@ -25,32 +29,61 @@ class TestCoroutineActivity : AppCompatActivity() {
 
 
             //방법1, 비동기 작업, Handler 방식, deprecated , 참고만,
-            val handler = object : Handler() {
-                override fun handleMessage(msg: Message) {
-                    super.handleMessage(msg)
-                    binding.resultCoView.text = "time: ${msg.arg2}, sum : ${msg.arg1}"
-                }
-            }
+//            val handler = object : Handler() {
+//                override fun handleMessage(msg: Message) {
+//                    super.handleMessage(msg)
+//                    binding.resultCoView.text = "time: ${msg.arg2}, sum : ${msg.arg1}"
+//                }
+//            }
 
             // thread 작업, 비동기 작업. 코루틴에 비해서 성능이 조금 무겁다.
-            thread {
-                // 무거운 작업(오래 걸리는 작업 넣기.)
+//            thread {
+//                // 무거운 작업(오래 걸리는 작업 넣기.)
+//                var sum = 0L
+//                var time = measureTimeMillis {
+//                    for(i in 1..9_000_000_000){
+//                        sum += i
+//                    }
+//                }
+//                //  오래 걸리는 작업의 결과값을 메세지를 통해서, 핸들러에게 전달하는 로직.
+//                val message = Message()
+//                message.arg1 = sum.toInt()
+//                message.arg2 = time .toInt()
+//                handler.sendMessage(message)
+//
+//                Log.d("lsy","time : $time")
+//            }
+
+
+            // 방법2, 코루틴 이용해서, 비동기 처리 해보기.
+            // 무거운 작업의 결괏값을 전달하는 도구.
+            val channel = Channel<Int> ()
+            // 무건운 작업을 수행하는 로직.
+            // Dispatchers.Default : 백그라운드 CPU 연산을 많이 할 때 사용하는 케이스
+            val backgroundScop = CoroutineScope(Dispatchers.Default + Job())
+            backgroundScop.launch {
+                // 무거운 작업 추가.
                 var sum = 0L
                 var time = measureTimeMillis {
                     for(i in 1..9_000_000_000){
                         sum += i
                     }
                 }
-                //  오래 걸리는 작업의 결과값을 메세지를 통해서, 핸들러에게 전달하는 로직.
-                val message = Message()
-                message.arg1 = sum.toInt()
-                message.arg2 = time .toInt()
-                handler.sendMessage(message)
-
                 Log.d("lsy","time : $time")
+                channel.send(sum.toInt())
+//                channel.send(time.toInt())
+            }
+
+            // 화면 출력을 담당하는 MAIN 을 구축.
+            // 무거운 작업에서 결괏값을 받아서 출력을 함.
+            val mainScope = GlobalScope.launch(Dispatchers.Main) {
+                channel.consumeEach {
+                    binding.resultCoView.text = "sum : $it"
+                }
             }
 
         }
+        // testCoBtn setOnClick
 
 
 
