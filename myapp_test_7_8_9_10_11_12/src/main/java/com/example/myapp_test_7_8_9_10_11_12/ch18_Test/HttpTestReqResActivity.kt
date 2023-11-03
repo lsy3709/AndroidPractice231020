7182864aab862f8d4c6ebcf6f7da0a8676b88f97
lsy3709 <lsy3709@naver.com>
2023-11-03 12:50:03 +0900

    231103 4교시 18 reqres.in 사이트에서 기본 유저 정보 받아와서 리사이클러뷰 출력하기, 작업4)데이터 가져오기1

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch18_Test/HttpTestReqResActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch18_Test/HttpTestReqResActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapp_test_7_8_9_10_11_12.ch18_Test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapp_test_7_8_9_10_11_12.ch18_Test.retrofit.MyApplication
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityHttpTestReqResBinding
 
 class HttpTestReqResActivity : AppCompatActivity() {
@@ -52,6 +53,20 @@ class HttpTestReqResActivity : AppCompatActivity() {
         // 바로 사용만 하면 됨.
 
         // 작업 순서4: 일단, retrofit 통신 이용해서 데이터가 전달 유무 확인.
+        // 데이터 가져오기
+        // 1)
+        // applicationContext 안에 우리가 등록한 설정이 있고,
+        // as MyApplication -> 형변환
+        // 형변환 된 인스턴스 내부에 networkService 를 사용하기.
+        val networkService = (applicationContext as MyApplication).networkService
+
+        // 2) 호출하는 함수 콜 만들기. 우리가 만든 인터페이스에 등록된
+        // 추상 함수를 이용함. 인자값은 페이지 번호 정의를 문자열 타입으로 했음.
+        val userListCall = networkService.doGetUserList("2")
+
+        // 실제 통신이 시작이 되는 부분, 이 함수를 통해서 데이터를 받아옴.
+        userListCall.enqueue()
+
 
         // 작업 순서5 : 리사이클러뷰에 넣는 작업. , 재사용.
 
