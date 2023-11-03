f08dcced974317d8cc6898f9a9711fea92612c4c
lsy3709 <lsy3709@naver.com>
2023-11-03 14:17:55 +0900

    231103 5교시 18 reqres.in 사이트에서 기본 유저 정보 받아와서 리사이클러뷰 출력하기, 작업5) 리사이클러뷰 붙이기, 목록요소 뷰 구성

---
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch18_Test/adapter/MyAdapterRetrofit.kt
@@ -0,0 +1,11 @@
+package com.example.myapp_test_7_8_9_10_11_12.ch18_Test.adapter
+
+
+// 매개변수에는, 목록 요소의 뷰가 ,
+// 이름 : item_retrofit.xml
+// 왼쪽 : 이미지 하나, 오르쪽에 : 세로 방향으로 텍스뷰 3개정도.
+class MyViewHolderRetrofit (val binding: )
+
+// 리사이클러뷰 준비물 1) 어댑터 2) 뷰홀더 3) 목록 요소의 뷰 4) 데이터
+class MyAdapterRetrofit {
+}
