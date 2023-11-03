46c2dcb984f547126774c07750d2e726e9af6df6
lsy3709 <lsy3709@naver.com>
2023-11-03 15:14:50 +0900

    231103 5교시 18 reqres.in 사이트에서 기본 유저 정보 받아와서 리사이클러뷰 출력하기, 작업5) 리사이클러뷰 붙이기, 어댑터 구성 작업3, 액티비티 빈화면에 리사이클러뷰 붙이기, 어댑터 붙이기

--- myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch18_Test/HttpTestReqResActivity.kt
+++ myapp_test_7_8_9_10_11_12/src/main/java/com/example/myapp_test_7_8_9_10_11_12/ch18_Test/HttpTestReqResActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapp_test_7_8_9_10_11_12.ch18_Test
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapp_test_7_8_9_10_11_12.ch18_Test.adapter.MyAdapterRetrofit
 import com.example.myapp_test_7_8_9_10_11_12.ch18_Test.model.UserListModel
 import com.example.myapp_test_7_8_9_10_11_12.ch18_Test.retrofit.MyApplication
 import com.example.myapp_test_7_8_9_10_11_12.databinding.ActivityHttpTestReqResBinding
@@ -79,6 +81,15 @@ class HttpTestReqResActivity : AppCompatActivity() {
                 Log.d("lsy","userList의 값 : ${userList?.data}")
 
             // 데이터를 성공적으로 받았다면, 여기서 리사이클러 뷰 어댑터에 연결하면 됨.
+                // 리사이클러뷰 의 레이아웃 정하는 부분, 기본인 LinearLayoutManager 이용했고,
+                val layoutManager = LinearLayoutManager(
+                    this@HttpTestReqResActivity)
+                // 리사이클러뷰에 어댑터 연결
+                // 인자값은 : 현재 context : this@HttpTestReqResActivity
+                // 2번째 인자값은 : 데이터 , 네트워크 ,레트로핏2 통신으로 받아온 데이터 리스트
+                binding.retrofitRecyclerView.layoutManager = layoutManager
+                binding.retrofitRecyclerView.adapter =
+                    MyAdapterRetrofit(this@HttpTestReqResActivity,userList?.data)
 
             }
 
